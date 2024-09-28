8b35d9902d3d4360b55f1814b97bfe5b7b33dabe
tapa991 <tapa@sheridancollege.ca>
2024-09-28 19:38:04 -0400

    Initialized project

---
+++ PatrickT-Assignment-1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PatrickT_Assignment_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
