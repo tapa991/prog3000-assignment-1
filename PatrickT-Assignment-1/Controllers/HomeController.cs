730f996c5ea1d7406d9438a5f0e8d29dd03e2c1b
tapa991 <tapa@sheridancollege.ca>
2024-09-28 21:43:52 -0400

    Added available equipment to the available equipment page

--- PatrickT-Assignment-1/Controllers/HomeController.cs
+++ PatrickT-Assignment-1/Controllers/HomeController.cs
@@ -21,31 +21,13 @@ namespace PatrickT_Assignment_1.Controllers
 
         public IActionResult AllListings()
         {
-            var equipment = new List<Equipment>
-            {
-                new Equipment {Id  = 1, Description = "Lenovo Thinkpad", Available = true},
-                new Equipment {Id  = 2, Description = "Chromebook", Available = false},
-                new Equipment {Id  = 3, Description = "IPhone", Available = true},
-                new Equipment {Id  = 4, Description = "Google Pixel", Available = true},
-                new Equipment {Id  = 5, Description = "IPad", Available = false},
-                new Equipment {Id  = 6, Description = "IPad Mini", Available = false},
-                new Equipment {Id  = 7, Description = "Raspberry Pi", Available = true},
-            };
+            var equipment = GetEquipmentList();
             return View(equipment);
         }
 
         public IActionResult AvailableEquipment()
         {
-            var equipment = new List<Equipment>
-            {
-                new Equipment {Id  = 1, Description = "Laptop", Available = true},
-                new Equipment {Id  = 2, Description = "Laptop", Available = false},
-                new Equipment {Id  = 3, Description = "Laptop", Available = true},
-                new Equipment {Id  = 4, Description = "Laptop", Available = true},
-                new Equipment {Id  = 5, Description = "Laptop", Available = false},
-                new Equipment {Id  = 6, Description = "Laptop", Available = false},
-                new Equipment {Id  = 7, Description = "Laptop", Available = true},
-            };
+            var equipment = GetEquipmentList();
             return View(equipment);
         }
 
@@ -72,6 +54,20 @@ namespace PatrickT_Assignment_1.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private List<Equipment> GetEquipmentList()
+        {
+            return new List<Equipment>
+            {
+                new Equipment {Id  = 1, Description = "Lenovo Thinkpad", Available = true},
+                new Equipment {Id  = 2, Description = "Chromebook", Available = false},
+                new Equipment {Id  = 3, Description = "IPhone", Available = true},
+                new Equipment {Id  = 4, Description = "Google Pixel", Available = true},
+                new Equipment {Id  = 5, Description = "IPad", Available = false},
+                new Equipment {Id  = 6, Description = "IPad Mini", Available = false},
+                new Equipment {Id  = 7, Description = "Raspberry Pi", Available = true},
+            };
         }
     }
 }
